5b03aca913d0275ae81270b326843f163c8d4217
Michael-IRL <iszekeell@gmail.com>
2017-11-30 22:49:35 +0000

    Change base file into notes for jQuery

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 /*
+* I'm useing this as a template to hold my jQuery notes 
+* If you are reading this please check out.
+*
+* https://classroom.udacity.com/courses/ud245
+* 
+* The above course is great for strating out with jQery
 
-For this quiz, can you use this script, which is linked in the <head> of index.html,
-to change the boring placeholder image to a picture of a cute animal?
 
-Remember, you'll need to pass a function into the jQuery object to run
-when the document is ready.
 
-Unfortunately, placepuppy is no longer available. Here's a link to a random
-animal image on lorempixel.com:
-http://lorempixel.com/350/150/animals/
 
-Good luck!
 
+This was the last quiz of lesson 3 
 */
 // create a function to be called when the DOM loads
 function changePicture()
